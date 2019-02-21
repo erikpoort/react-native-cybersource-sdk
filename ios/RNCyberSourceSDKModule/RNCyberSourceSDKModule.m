3dbd0758ff3db0775957c229e94e85ad2120c3b2
Erik Poort <hello@erikpoort.nl>
2019-02-21 21:39:20 +0100

    Create and map configuration call on iOS

--- ios/RNCyberSourceSDKModule/RNCyberSourceSDKModule.m
+++ ios/RNCyberSourceSDKModule/RNCyberSourceSDKModule.m
@@ -7,12 +7,40 @@
 //
 
 #import "RNCyberSourceSDKModule.h"
+#import <TrustDefender/TrustDefender.h>
 
 static NSString *const kRejectCode = @"CyberSourceSDKModule";
 
 @implementation RNCyberSourceSDKModule {
+    THMTrustDefender *_defender;
 }
 
 RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(
+    configure:(NSString *)orgId
+    serverURL:(NSString *)serverURL
+    resolver:(RCTPromiseResolveBlock)resolve
+    rejecter:(RCTPromiseRejectBlock)reject
+) {
+    if (_defender) {
+        reject(kRejectCode, @"CyberSource SDK is already initialised", nil);
+        return;
+    }
+    
+    _defender = [THMTrustDefender sharedInstance];
+    
+    @try {
+        [_defender configure:@{
+            THMOrgID: orgId,
+            THMFingerprintServer: serverURL,
+        }];
+    } @catch (NSException *exception) {
+        reject(kRejectCode, @"Invalid parameters", nil);
+        return;
+    }
+    
+    resolve(@YES);
+}
 
 @end
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import { NativeModules } from 'react-native';
 const { RNCyberSourceSDKModule } = NativeModules;
 
+
+/**
+ * Configure
+ * @param {string} orgId OrgId
+ * @param {string} serverURL The fingerprint url including orgId and sessionId according to the
+ *     documentation.
+ * @return {promise}
+ */
+function configure(orgId, serverURL) {
+  return RNCyberSourceSDKModule.configure(orgId, serverURL);
+}
+
 module.exports = {
+  configure,
 }
