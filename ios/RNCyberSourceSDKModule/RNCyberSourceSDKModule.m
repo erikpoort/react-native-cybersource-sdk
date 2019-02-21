ede2a0a317943cfbb50e57597768ecb33ef67f59
Erik Poort <hello@erikpoort.nl>
2019-02-21 20:31:13 +0100

    Add ios default template

---
+++ ios/RNCyberSourceSDKModule/RNCyberSourceSDKModule.m
@@ -0,0 +1,18 @@
+//
+//  RNCyberSourceSDKModule.m
+//  RNCyberSourceSDKModule
+//
+//  Created by Erik Poort on 21/02/2019.
+//  Copyright (c) 2019 Erik Poort. All rights reserved.
+//
+
+#import "RNCyberSourceSDKModule.h"
+
+static NSString *const kRejectCode = @"CyberSourceSDKModule";
+
+@implementation RNCyberSourceSDKModule {
+}
+
+RCT_EXPORT_MODULE();
+
+@end
